8a47e4eb9e4e4558152a2bcc4dc21273c40b4a57
abhilashg562@gmail.com <abhilashgs14@gmail.com>
2026-02-20 00:26:28 +0530

    Backend authentication(bcrypt,jsonwebtoken)

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -7,9 +9,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.post("/test", (req, res) => {
+  res.json({ message: "POST working" });
+});
+
 // Only load providers route for now
 app.use("/api/providers", require("./routes/providers"));
-
+app.use("/api/auth", require("./routes/auth"));
 // Health check
 app.get("/", (req, res) => {
   res.json({ status: "GigMarket API running 🚀" });
